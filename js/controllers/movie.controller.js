12603c1cc5b2f62f5049d0f659c2f7ce8a6ad05c
uriel87 <urielch87@gmail.com>
2017-11-21 19:24:22 +0200

    start movie page for order moive

--- js/controllers/movie.controller.js
+++ js/controllers/movie.controller.js
@@ -25,6 +25,37 @@ app.controller('movieCtl', ['$scope', 'movieService','$location','$timeout', fun
         $scope.lastCommentReview = $scope.movieReview[0].reviews[$scope.movieReview[0].reviews.length - 1];
     });
 
+    $scope.getReviewPercentage = function (review) {
+        return (review / $scope.getTotalReview()) * 100;
+    }
+
+    $scope.getTotalReview = function () {
+        // setTimeout(function() {
+            return parseInt($scope.movieReview[0].wtf) +
+                parseInt($scope.movieReview[0].lame) +
+                parseInt($scope.movieReview[0].wow) +
+                parseInt($scope.movieReview[0].nice)
+        // }, 1000);
+
+    }
+
+    $scope.getReviewPercentage = function (review) {
+
+        //setTimeout(function() {
+            if(!review) {
+                widthPrecetage = 0;
+            } else {
+                widthPrecetage = (review / $scope.getTotalReview()) * 100;
+            }
+            widthRating = {
+                "width" : widthPrecetage + '%'
+            }
+            console.log("$scope.getTotalReview(): " + $scope.getTotalReview());
+            console.log("widthRating: " + JSON.stringify(widthRating));
+            return widthRating
+        //}, 50);
+    }
+
 }]);
 
 
